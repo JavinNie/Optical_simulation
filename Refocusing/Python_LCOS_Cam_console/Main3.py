2771dbc0e773795a777ed06373948382667dcfcc
JavinNie <30895641+JavinNie@users.noreply.github.com>
2021-11-11 17:54:07 +0800

    Add files via upload
    zernike和3P模拟退火优化算法代码备份

--- Refocusing/Python_LCOS_Cam_console/Main3.py
+++ Refocusing/Python_LCOS_Cam_console/Main3.py
@@ -1,9 +1,9 @@
 '''
-Zernike系数15，模拟退火
+双锥菲涅尔透镜，模拟退火
+fx,fy,angle
 2021-11-11
 Nie Jiewen
 '''
-
 
 import sys
 from PyQt5 import QtGui, QtCore
@@ -16,12 +16,9 @@ import cv2
 from Mainwindow import Ui_MainWindow
 import time
 import xlwt
-from zernike import RZern
-from scipy import signal
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
-
     def __init__(self):
         super(MainWindow, self).__init__()  # 父类构造函数，可以调用父类的属性
         self.setupUi(self)
@@ -64,13 +61,12 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.SLM_scene = QtWidgets.QGraphicsScene()  # 控制台 scene
         self.SLM_Init()
 
-        self.Hologram = np.random.randint(254, 255, [401, 401], np.uint8)  # initial
+        self.Hologram = np.random.randint(254, 255, [200, 200], np.uint8)  # initial
         self.HologramPositionTop = int(self.Top_spinBox.value())
         self.HologramPositionLeft = int(self.Left_spinBox.value())
 
         self.Max_Hologram = self.Hologram
 
-        # Slot bound
         self.Slot_bound()
         # Auto之后必然是自定义自动刷新的函数Auto_clicked(),这里为了简单显示效果采取调用显示接口的函数，
         # 实际上显示接口应该只是一部分, 还有更新全息图，更新显示以及，获取相机值的作用
@@ -210,47 +206,18 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         # position_left = self.Aim_hor_spinBox.value()
         # position_top = self.Aim_ver_spinBox.value()
         # scale = self.Aim_Scale_spinBox.value()
-
-        # self.cam_image=self.cam_acquisition()
-
-        # print('Time cost = %fs' % (time.time() - self.time))
-
-        # 自动调整目标选区，但是直接卷积太慢了6s/f，
-        # 可以尝试三步法确定一个小范围，然后再在一个小范围内卷积确定精确坐标
-        # if self.auto_flag == 1:
-        #     # 窗口实施刷新，后台计算不影响界面刷新
-        #     QtWidgets.QApplication.processEvents()
-        #     # 计时
-        #     self.time = time.time()
-        #     # 计算全局最大的值
-        #     # 设计一个函数，输入图像，及每边分割数4？，然后自己迭代到目标选区
-        #     # 输出一个小区域及起始坐标，然后可以对这个小区域做卷积
-        #     # 均值卷积核生成
-        #     nurcle=np.ones((self.scale,self.scale))/self.scale/self.scale
-        #     # 尺寸不变，卷积
-        #     cam_image_temp=signal.convolve2d(self.cam_image, nurcle,'same')
-        #     print('Time cost = %fs' % (time.time() - self.time))
-        #     # 全局最大值,及坐标
-        #     value_temp=np.max(cam_image_temp)
-        #     ind_max=np.where(cam_image_temp == np.max(cam_image_temp))
-        #     if value_temp>self.Max_aim_value:
-        #         # 更新最大值及目标区坐标
-        #         self.Max_aim_value=value_temp
-        #         self.Aim_hor_spinBox.setProperty("value", ind_max[1][0])
-        #         self.Aim_ver_spinBox.setProperty("value", ind_max[0][0])
-        #         self.Aim_area_update()
-
-        self.hot_area_search()
-
         position_left = self.position_left
         position_top = self.position_top
         scale = self.scale
+
         # 目标区域的 mean value, H代表水平方向上，即横坐标，x坐标，left坐标，V代表垂直方向上，即纵坐标，y坐标，top坐标
         Hmin = np.clip(position_left - scale, 0, self.cam_image.shape[1] - 1)
         Hmax = np.clip(position_left + scale, 1, self.cam_image.shape[1])
         Vmin = np.clip(position_top - scale, 0, self.cam_image.shape[0] - 1)
         Vmax = np.clip(position_top + scale, 1, self.cam_image.shape[0])
-        aim_area_value = int(np.mean(self.cam_image[Vmin:Vmax, Hmin:Hmax]))
+        # 计算目标区域的值
+        aim_area_value = int(np.mean(
+            self.cam_image[Vmin:Vmax, Hmin:Hmax]))
         if self.initial_flag == 1:
             self.Initial_lineEdit.setText(str(aim_area_value))
             self.initial_flag = 0
@@ -258,9 +225,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.Latest_lineEdit.setText(str(aim_area_value))  # 最新值
         self.Promotion_lineEdit.setText(str(int(aim_area_value) - int(self.last_aim_value)))  # 提升值
         self.last_aim_value = aim_area_value
-
-        # self.Max_aim_value##历史最大值
-
+        #
         # # time
         # print('Time cost = %fs' % (time.time() - self.time))
         # self.time = time.time()
@@ -283,36 +248,6 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.cam_scene.addItem(item)
         # self.cam_scene.update()
         self.Camera.setScene(self.cam_scene)
-
-    def hot_area_search(self):
-        self.cam_image = self.cam_acquisition()
-        scale = self.scale
-        r, c = self.cam_image.shape
-        nr = r // scale
-        nc = c // scale
-        result = []
-        for ii in range(nr):
-            for jj in range(nc):
-                r0 = ii * scale
-                r1 = (ii + 1) * scale - 1
-                c0 = jj * scale
-                c1 = (jj + 1) * scale - 1
-                ave = np.mean(self.cam_image[r0:r1, c0:c1])
-                result.append([ii, jj, ave])
-        result = np.array(result)
-        index_max = np.argmax(result[:, 2], axis=0)  # 竖着比较，返回最大值的索引
-        ii_max = result[index_max, 0]
-        jj_max = result[index_max, 1]
-        value_max = result[index_max, 2]
-        position_top = int((ii_max + 0.5) * scale)
-        position_left = int((jj_max + 0.5) * scale)
-        if value_max > self.Max_aim_value:
-            # 更新最大值及目标区坐标
-            self.Max_aim_value = value_max
-            self.Aim_hor_spinBox.setProperty("value", position_left)
-            self.Aim_ver_spinBox.setProperty("value", position_top)
-            self.Aim_area_update()
-        # update ,self.position_left ,self.position_top
 
     def Cam_gray2color(self):
         self.cam_image = self.cam_acquisition()
@@ -349,7 +284,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def clear_clicked(self):
         # 1、清空 SLM显示器
-        self.Hologram = np.random.randint(254, 255, [401, 401], np.uint8)
+        self.Hologram = np.random.randint(254, 255, [200, 200], np.uint8)
         self.SLM_display()
         # 2、Auto程序清空，关闭自动定时器，开始相机定时器
         self.End_clicked()
@@ -357,14 +292,18 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def Auto_clicked(self):
         # 开始重新计算初始值
         self.initial_flag = 1
+
         if self.auto_flag == 0:
             self.auto_flag = 1
             self.timer_camera.stop()  # 关闭相机自动刷新计时器ms
             self.SLM_position_update()  # 全息图位置参数更新
+        # 窗口实施刷新，后台计算不影响界面刷新
+        QtWidgets.QApplication.processEvents()
         self.Auto_model()
 
     def Auto_model(self):
-        self.Max_Hologram = self.Hologram  # 在现有基础上优化
+        # 窗口实施刷新，后台计算不影响界面刷新
+        QtWidgets.QApplication.processEvents()
         self.SA_for_flen()
 
     def SA_for_flen(self):
@@ -375,27 +314,25 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         runtime_start = time.time()  # 运行时间起始点
 
         # SA参数设置
-        T_init = 1000  # 初始最大温度
+        T_init = 10000  # 初始最大温度
         alpha = 0.90  # 降温系数
-        T_min = 450  # 最小温度，即退出循环条件
+        T_min = 500  # 最小温度，即退出循环条件
 
-        length_Coefficient = 15
-        scale_lower = -1
-        scale = 2
-        scale_limit = scale * np.ones(length_Coefficient)
-        scale_limit_lower = np.ones(length_Coefficient) * scale_lower
-        step = 0.48 * scale_limit
+        scale_upper = 1000
+        scale_lower = -1000
+        step = 0.48 * (scale_upper - scale_lower)  # 随机扰动步长
 
-        # (x-scale_limit_lower)%scale_limit+scale_limit_lower
-        #
-        # cart.eval_grid(zernike_Coefficient, matrix=True)
+        ang_scale_upper = 360
+        ang_scale_lower = 0
+        step_ang = 0.48 * (ang_scale_upper - ang_scale_lower)  # 随机扰动步长
 
-        search_depth = 35  # 搜索深度
+        search_depth = 40  # 搜索深度
         T = T_init  #
 
         # 写入excel表格
         workbook = xlwt.Workbook()
         sheet = workbook.add_sheet("Sheet Name1")
+
 
         while self.auto_flag == 1:
             # 窗口实施刷新，后台计算不影响界面刷新
@@ -403,106 +340,118 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
             results = []  # 存x，y
             count = 0
-            # x=step
-            x = np.zeros(length_Coefficient)
+            x1 = 100  # 初始位置
+            x2 = 100
             ang = 0
-            self.update_flens(x)  # 更新flens全息图
+            self.update_flens(x1, x2, ang)  # 更新flens全息图
             y = self.last_aim_value
+            sheet.write(Iteration, 0, str(x1))
+            sheet.write(Iteration, 1, str(x2))
+            sheet.write(Iteration, 2, str(y))
             while (T > T_min) & (self.auto_flag == 1):
-                x_best = x
+                x1_best = x1
+                x2_best = x2
+                ang_best = ang
+
                 y_best = y  # 设置成这个收敛太快了，令人智熄
                 flag = 0  # 用来标识该温度下是否有新值被接受
                 # 扰动步长递减
-                # step = step * T / T_init
+                step = step * T / T_init
                 # 每个温度迭代多次，找最优解
                 for i in range(search_depth):
                     if self.auto_flag != 1:
                         break
-                    delta_x = np.random.randn(length_Coefficient) * step  # 自变量1进行扰动
-                    x_new = ((x + delta_x) - scale_limit_lower) % scale_limit + scale_limit_lower  # 进行限幅
-                    self.update_flens(x_new)  # 更新flens全息图
+                    delta_x1 = np.random.randn() * step  # 自变量1进行扰动
+                    delta_x2 = np.random.randn() * step  # 自变量2进行扰动
+                    delta_ang = np.random.randn() * step_ang  # 自变量3进行扰动
+                    # 自变量1变化后仍要求在[0,10]之间
+                    if scale_lower < (x1 + delta_x1) < scale_upper:
+                        x1_new = x1 + delta_x1
+                    elif scale_lower < (x1 - delta_x1) < scale_upper:
+                        x1_new = x1 - delta_x1
+                    else:
+                        x1_new = x1
+                    # 自变量2变化后仍要求在[0,10]之间
+                    if scale_lower < (x2 + delta_x2) < scale_upper:
+                        x2_new = x2 + delta_x2
+                    elif scale_lower < (x2 - delta_x2) < scale_upper:
+                        x2_new = x2 - delta_x2
+                    else:
+                        x2_new = x2
+                    # 自变量3变化后仍要求在[0,10]之间
+                    if ang_scale_lower < (ang + delta_ang) < ang_scale_upper:
+                        ang_new = ang + delta_ang
+                    elif ang_scale_lower < (ang - delta_ang) < ang_scale_upper:
+                        ang_new = ang - delta_ang
+                    else:
+                        ang_new = ang
+
+                    self.update_flens(x1_new, x2_new, ang_new)  # 更新flens全息图
                     y_new = self.last_aim_value
+
+
                     if self.update_Exposure_flag == 1:
-                        self.update_flens(x)  # 更新flens全息图
+                        self.update_flens(x1, x2, ang)  # 更新flens全息图
                         y = self.last_aim_value
-                        self.update_flens(x_best)  # 更新flens全息图
+                        self.update_flens(x1_best, x2_best, ang_best)  # 更新flens全息图
                         y_best = self.last_aim_value
                         self.update_Exposure_flag = 0
 
                     # 以上为找最大值，要找最小值就把>号变成<
                     if y_new >= y or np.exp(-(y - y_new) / T) > np.random.rand():
                         flag = 1  # 有新值被接受
-                        x = x_new
+                        x1 = x1_new
+                        x2 = x2_new
+                        ang = ang_new
                         y = y_new
                         if y > y_best:  # 改变最佳值记录
-                            x_best = x
+                            x1_best = x1
+                            x2_best = x2
+                            ang_best = ang
                             y_best = y
 
-                    print(str(count) + ' ' + str(y_new) + ' ' + str(x_new))
+                    print(str(count) + ' ' + str(x1_new) + ' ' + str(x2_new) + ' ' + str(ang_new) + ' ' + str(y_new))
                     count = count + 1
-                    # 写入excel表格
-                    # Iteration = Iteration + 1
                 if flag:
-                    x = x_best
+                    x1 = x1_best
+                    x2 = x2_best
+                    ang = ang_best
                     y = y_best
                 T *= alpha  # 温度下降
                 # 写入excel表格
                 Iteration = Iteration + 1
-                # sheet.write(Iteration, 0, str(x1))
-                # sheet.write(Iteration, 1, str(x2))
-                sheet.write(Iteration, 1, str(y_best))
-            self.update_flens(x_best)
-            self.update_flens(x_best)
-            print(str(count) + ' ' + str(y_best) + ' ' + str(x_best))
-            workbook.save('Lens_Value_zernike.xls')
+                sheet.write(Iteration, 0, str(x1))
+                sheet.write(Iteration, 1, str(x2))
+                sheet.write(Iteration, 2, str(y_best))
+
+
+            self.update_flens(x1_best, x2_best, ang_best)
+            workbook.save('Lens_Value_3P.xls')
+
             self.End_clicked()
 
-    def update_flens(self, zernike_Coefficient):
+    def update_flens(self, fx_lens, fy_lens, angle):
         # 窗口实施刷新，后台计算不影响界面刷新
         QtWidgets.QApplication.processEvents()
-
-        m1 = 0.0205 * np.pi
-        m2 = 2.4219 * np.pi
-        m_phase = np.arange(0, 256) / 255 * (m2 - m1) + m1
-        m_grey = np.arange(0, 256)
-
+        # angle 单位是角度
         # 图像参数设置
         # f_lens = 200  # 250#-1 / 22  # mm
         width_Hologram = 200
         # 绘图坐标基础
         X, Y = np.meshgrid(np.arange(-width_Hologram, width_Hologram + 1),
                            np.arange(-width_Hologram, width_Hologram + 1))  # 2生成绘制3D图形所需的网络数据
+        R = (X ** 2 + Y ** 2) ** 0.5
 
-        ### 更新初始3p菲涅尔透镜的参数
-        fx_lens = -412.95700967814145
-        fy_lens = -493.0480790582172
-        angle = 257.7195305221332
         angle = np.pi * angle / 180
         T = [[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]]
         C = np.array([X, Y])
         [X, Y] = np.dot(T, C.reshape((2, X.size))).reshape((2, X.shape[0], X.shape[1]))
-        Max_phase = (X ** 2 / fx_lens + Y ** 2 / fy_lens)
 
-        ### zernike参数
-        cart = RZern(4)
-        cart.make_cart_grid(X / width_Hologram, Y / width_Hologram, unit_circle=False)
-        zernike_Coefficient = zernike_Coefficient * 10
-        if self.initial_flag == 1:
-            zernike_Coefficient = np.zeros(len(zernike_Coefficient))
-            # self.initial_flag == 0
-
-        zernike_Coefficient[0:3] = np.zeros(3)  # 前三项置零
-        phi = cart.eval_grid(zernike_Coefficient, matrix=True)
-        phi = phi + Max_phase
-        output_phase = np.angle(np.exp(1j * phi)) + np.pi
-
-        # R = (X ** 2 + Y ** 2) ** 0.5
-        # angle = np.pi * angle / 180
-        # T = [[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]]
-        # C = np.array([X, Y])
-        # [X, Y] = np.dot(T, C.reshape((2, X.size))).reshape((2, X.shape[0], X.shape[1]))
-        # output_phase = np.angle(np.exp(1j * (X ** 2 / fx_lens + Y ** 2 / fy_lens))) + np.pi
-
+        output_phase = np.angle(np.exp(1j * (X ** 2 / fx_lens + Y ** 2 / fy_lens))) + np.pi
+        m1 = 0.0205
+        m2 = 2.4219
+        m_phase = np.arange(0, 256) / 255 * (m2 - m1) + m1
+        m_grey = np.arange(0, 256)
         m_bitmap = np.uint8(np.interp(output_phase, m_phase, m_grey))
         # 更新hologram
         self.Hologram = m_bitmap
@@ -515,8 +464,10 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         #     self.update_Exposure_flag = 1
 
     def reset_Cam(self):
+
         self.update_cam_value()
         EX_value = self.Exposure_spinBox.value()
+
         # 曝光值的调节左右侧
         left = 0
         right = 170
@@ -550,29 +501,29 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         # 调用此函数前必须更新 self.Hologram
 
         # 2、更新全息图位置参数和SLM显示器
+
         self.SLM_display()
+
         # 窗口实施刷新，后台计算不影响界面刷新
         QtWidgets.QApplication.processEvents()
         # 3、刷新相机，更新相机目标区域的值 self.last_aim_value
         # self.update_cam_value()
         for i in range(8):
+            # self.time = time.time()
             self.Cam_display()
+            # print('Time cost = %fs' % (time.time() - self.time))
+
             # 窗口实施刷新，后台计算不影响界面刷新
             QtWidgets.QApplication.processEvents()
 
     def End_clicked(self):
         self.auto_flag = 0
-        self.initial_flag = 1
-        self.Max_aim_value =0
         self.timer_camera.start(50)  # 开启自动程序计时器ms，顺序刷新取值
 
     def Save_clicked(self):
-        cv2.imwrite('Result_picture/Max_camera_10m_3P+zernike_45mA_Exp7.bmp', self.Hologram)
+        cv2.imwrite('Max_camera_20m_3P_45mA_Exp20_0.bmp', self.Hologram)
         self.cam_image = self.cam_acquisition()
-        cv2.imwrite('Result_picture/Max_camera_10m_3P+zernike_45mA_Exp7.png',
-                    cv2.applyColorMap(self.cam_image, cv2.COLORMAP_JET))
-        # cv2.imwrite('Max_camera_20m_clear_45mA_Exp170.png',
-        #           cv2.applyColorMap(self.cam_image, cv2.COLORMAP_JET))
+        cv2.imwrite('Max_camera_20m_3P_45mA_Exp20_0.png', cv2.applyColorMap(self.cam_image, cv2.COLORMAP_JET))
 
     def SLM_position_update(self):
         self.HologramPositionTop = int(self.Top_spinBox.value())
@@ -580,6 +531,8 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def Hologram_Auto_genaration(self):
         Hologram = np.random.randint(0, 255, [800, 800], np.uint8)
+        # f=
+        # Hologram
         # 改成遗传算法
         return Hologram
 
